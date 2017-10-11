26d6f73ea73f9047ea9908de66824a55271422a4
nehaesfera <31731054+nehaesfera@users.noreply.github.com>
2017-10-11 14:43:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,17 +13,19 @@ $catList = $ali->getListCategory();
 //get product list by categoryID
 //sort: commissionRateDown
 //filter: commissionRateFrom from 0.5
-$productListByCategory = $ali->getListProduct(100002612, array('sort'=>'commissionRateDown', 'commissionRateFrom'=>0.5));
+//$productListByCategory = $ali->getListProduct(100002612, array('sort'=>'commissionRateDown', 'commissionRateFrom'=>0.5));
 
 //get product list by Keyword
-$productListByKeyword = $ali->getListProduct('shoes');
+//$productListByKeyword = $ali->getListProduct('shoes');
 
 //get product list by CategoryID and Keyword
 //filter: commissionRateFrom from 0.5
-$productList = $ali->getListProduct(322, array('keywords'=>'man shoes', 'commissionRateFrom'=>0.5));
+//$productList = $ali->getListProduct(322, array('keywords'=>'man shoes', 'commissionRateFrom'=>0.5));
 
 //get product by ID
-$product = $ali->getProduct(32213749383);
+$product = $ali->getProduct(32774314225);
+
+print_r($product);
 
 //get promotion link(s)
-$link = $ali->getPromotionLinks('<your_tracking_ID>', "http://www.aliexpress.com/item//32213749383.html,http://www.aliexpress.com/item//1786034050.html");
+//$link = $ali->getPromotionLinks('<your_tracking_ID>', "http://www.aliexpress.com/item//32213749383.html,http://www.aliexpress.com/item//1786034050.html");
